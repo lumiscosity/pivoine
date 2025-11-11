3c07fba8c8d62075a17c1bfa1b7fca1b0ea1b161
lumiscosity <averyrudelphe@gmail.com>
2025-11-11 17:11:24 +0100

    bump qt6 + minor style tweak

--- src/reservation_list.h
+++ src/reservation_list.h
@@ -50,10 +50,10 @@ int run_reservation_list(std::string project, QWidget *parent) {
         // Saving the database without Maniacs clears the stringvar chunk
         // Commenting this out until this is resolved
 
-        //out << "STRING VARIABLES,"+QString::number(database->maniac_string_variables.size())+"\n";
-        //for (lcf::rpg::StringVariable i : database->maniac_string_variables) {
-        //    out << db_to_q(i.name).trimmed()+"\n";
-        //}
+        // out << "STRING VARIABLES,"+QString::number(database->maniac_string_variables.size())+"\n";
+        // for (lcf::rpg::StringVariable i : database->maniac_string_variables) {
+        //     out << db_to_q(i.name).trimmed()+"\n";
+        // }
 
         out << "TILESETS,"+QString::number(database->chipsets.size())+"\n";
         for (lcf::rpg::Chipset i : database->chipsets) {
